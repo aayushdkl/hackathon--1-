27c8969afbf8f7ad1396329f42322b8ccfc04c13
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2023-12-23 09:19:51 +0545

    files

---
+++ hackathon-master/admin_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="content">
+            <h3>Welcome <span>Admin</span></h3>
+            <h1>Please enter your Login Details</h1>
+            <a href= "login.php" class="btn">Login</a>
+            <a href= "register_form.php" class="btn">Register</a>
+            <a href="logout.php" class="btn">Logout</a>
+        </div>
+    </div>
+    
+</body>
+</html>
